6095383de56add2fd7dfed6f5d19e64d9a5cfe7f
Nyaguthii-C <nyaguthiigc@gmail.com>
2024-02-23 06:22:39 +0300

    modify users schema to accept emaol field

--- setup/users.js
+++ setup/users.js
@@ -2,8 +2,9 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, unique: true }
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, unique: true, required: true }
 });
 
 // Export the user schema
