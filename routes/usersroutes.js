9f2e6ed075716bee2e43d3c1b73268ef733f7d35
Nyaguthii-C <nyaguthiigc@gmail.com>
2024-02-26 06:50:13 +0300

    define post and get routes forusers

---
+++ routes/usersroutes.js
@@ -0,0 +1,29 @@
+// loginroutes
+const express = require('express');
+const router = express.Router();
+const User = require('../models/users');
+
+
+// Retrieve users from database
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Add a new user to database
+router.post('/users', async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+
+module.exports = router;
