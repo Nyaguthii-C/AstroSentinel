ce796d985fa41abc64a00761de226e29b98b33cb
Nyaguthii-C <nyaguthiigc@gmail.com>
2024-02-26 06:48:33 +0300

    define GET and POST routes for markers

---
+++ routes/markersroutes.js
@@ -0,0 +1,42 @@
+// markersroutes.js
+const express = require('express');
+const router = express.Router();
+const Marker = require('../models/markers'); // Import marker model
+
+
+// Retrieve markers
+router.get('/get-marker', async (req, res) => {
+  try {
+    const markers = await Marker.find();
+    res.json(markers);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// add markers to map
+router.post('/add-marker', async (req, res) => {
+    try {
+        const { lat, lng, SQM_Reading, Bortle_Class } = req.body;
+
+        // Create a new marker instance
+        const newMarker = new Marker({
+            lat,
+            lng,
+            SQM_Reading,
+            Bortle_Class
+        });
+
+        // Save the marker data to your "markers" collection in MongoDB
+        await newMarker.save();
+
+        // Respond with a success message
+        res.status(200).json({ message: 'Marker added successfully' });
+    } catch (error) {
+        console.error('Error during marker addition:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+
+module.exports = router;
