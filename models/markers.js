82ff459ac17cc4988537a9e3926d072507dd11b3
Nyaguthii-C <nyaguthiigc@gmail.com>
2024-02-26 06:46:10 +0300

    define  database schema for markers

---
+++ models/markers.js
@@ -0,0 +1,13 @@
+// ./models/marker.js for handling markers schema
+const mongoose = require('mongoose');
+
+const markerSchema = new mongoose.Schema({
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+    SQM_Reading: { type: String, required: true },
+    Bortle_Class: { type: String }
+});
+
+const Marker = mongoose.model('Marker', markerSchema);
+
+module.exports = Marker;
