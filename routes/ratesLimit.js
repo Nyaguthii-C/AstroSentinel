c211c365a8bb5a287a3d582f804d14c02293903d
Nyaguthii-C <nyaguthiigc@gmail.com>
2024-03-18 03:38:09 +0300

    add rate limiting

---
+++ routes/ratesLimit.js
@@ -0,0 +1,10 @@
+// limit requests to a route
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 50, // limit each IP to 50 requests per window
+});
+
+
+module.exports = limiter;
