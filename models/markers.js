5cc32377b0b3204a1d9d96fecd5776d9822e3e4e
Nyaguthii-C <nyaguthiigc@gmail.com>
2024-03-22 23:20:27 +0300

    add date to marker schema

--- models/markers.js
+++ models/markers.js
@@ -5,7 +5,8 @@ const markerSchema = new mongoose.Schema({
     lat: { type: Number, required: true },
     lng: { type: Number, required: true },
     SQM_Reading: { type: String },
-    Bortle_Class: { type: String }
+    Bortle_Class: { type: String },
+    Reading_Day: {type: String, required: true }
 });
 
 const Marker = mongoose.model('Marker', markerSchema);
