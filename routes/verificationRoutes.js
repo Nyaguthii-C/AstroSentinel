a8149ec42502ab61af4ee15b5a02acec0e0bfb02
Nyaguthii-C <nyaguthiigc@gmail.com>
2024-03-29 23:22:52 +0300

    set verification method for user on signup

---
+++ routes/verificationRoutes.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/users');
+const bcrypt = require('bcrypt');
+
+// Verification route
+router.get('/verify/:token', async (req, res) => {
+  try {
+    // Extract the verification token from the URL parameters
+    const verificationToken = req.params.token;
+
+    // Find the user with the matching verification token
+    const user = await User.findOne({ verificationToken });
+
+    // If the user is found, mark their email address as verified
+    if (user) {
+      user.isEmailVerified = true;
+      user.verificationToken = undefined; // Clear the verification token
+      await user.save();
+      res.redirect('/login'); // Redirect to the login page or any other page
+    } else {
+      res.status(404).json({ error: 'Invalid verification token' });
+    }
+  } catch (error) {
+    console.error('Error during email verification:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+module.exports = router;
