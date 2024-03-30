611e8b7f4784bcc4015cd3bb4e9a932f6e9cca9e
Nyaguthii-C <nyaguthiigc@gmail.com>
2024-03-30 12:58:53 +0300

    script update

--- routes/ratesLimit.js
+++ routes/ratesLimit.js
@@ -3,7 +3,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 10, // limit each IP to 10 requests per window
+  max: 50, // limit each IP to 10 requests per window
 });
 
 
