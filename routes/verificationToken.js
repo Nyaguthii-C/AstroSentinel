d28cc35c49110ef719c876b59a5d2e98b2e83a57
Nyaguthii-C <nyaguthiigc@gmail.com>
2024-04-17 13:29:22 +0300

    encode token

--- routes/verificationToken.js
+++ routes/verificationToken.js
@@ -5,7 +5,11 @@ function generateVerificationToken(username, email) {
   // Generate a random string for additional randomness
   const randomString = Math.random().toString(36).substring(7);
 
-  const tokenData = username + email + randomString;
+  // Encode the token data to ensure URL safety
+  const tokenData = encodeURIComponent(username + email + randomString);
+
+  //const tokenData = username + email + randomString;
+
 
   // Hash the token data using bcrypt
   const verificationToken = bcrypt.hashSync(tokenData, 10);
