9dd29d0038a5d4a50c826734a90591daf5881bcf
Nyaguthii-C <nyaguthiigc@gmail.com>
2024-05-14 01:13:08 +0300

    remove console.log

--- middleware/authenticationMiddleware.js
+++ middleware/authenticationMiddleware.js
@@ -1,7 +1,5 @@
-// middleware/authenticationMiddleware.js
 // creates session for session based athentication
 const authenticationMiddleware = (req, res, next) => {
-    console.log('Session Data:', req.session);
     if (req.session && req.session.user) {
         // If session exists and user is logged in, proceed to the next middleware
         return next();
