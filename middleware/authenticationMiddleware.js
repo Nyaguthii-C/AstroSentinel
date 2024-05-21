5b99cec008cfa3fe13655735071778534db3191c
Nyaguthii-C <nyaguthiigc@gmail.com>
2024-05-21 23:57:10 +0300

    set redirection to next()

--- middleware/authenticationMiddleware.js
+++ middleware/authenticationMiddleware.js
@@ -2,9 +2,9 @@
 const authenticationMiddleware = (req, res, next) => {
     if (req.session && req.session.user) {
         // If session exists and user is logged in, proceed to the next middleware
-        //return next();
+        return next();
         //redirect to index page - data logging page
-        return res.redirect('/index');
+        //return res.redirect('/index');
     } else {
         // If session or user is not present, redirect to login page
         return res.redirect('/login');
